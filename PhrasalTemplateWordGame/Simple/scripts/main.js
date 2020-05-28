40aa8af8fc18e06269a51467811b11a876f4ddc0
Brian Haddad <brianhaddad@gmail.com>
2020-05-28 12:57:09 -0700

    Implemented first stage of functionality.

--- PhrasalTemplateWordGame/Simple/scripts/main.js
+++ PhrasalTemplateWordGame/Simple/scripts/main.js
@@ -1,3 +1,7 @@
 function main(templateData){
-    //console.log(templateData);
+    for (var i=0; i<templateData.inputs.length; i++){
+        var answer = prompt('Please input a ' + templateData.inputs[i]);
+        templateData.string = templateData.string.replace(templateData.inputs[i], answer);
+    }
+    document.getElementById('main').innerHTML = templateData.string;
 }

415e308deae069a54abae46b768b02c562100015
Brian Haddad <brianhaddad@gmail.com>
2020-05-31 14:49:20 -0700

    Broke the process into discreet steps that can be separated and can be separated out into different functions.
    This will allow us to ransomize the collection process and collect the data in a form.

--- PhrasalTemplateWordGame/Advanced/scripts/main.js
+++ PhrasalTemplateWordGame/Advanced/scripts/main.js
@@ -5,6 +5,7 @@ function main(templateDatas, id) {
 
     var userPrompts = [];
 
+    //Phase 1: Process template, gather prompt data, and modify template.
     for (var i = 0; i < templateDatas.length; i++) {
         var templateData = templateDatas[i];
         if (!templateData.inputs || templateData.inputs.length < 1) {
@@ -15,23 +16,37 @@ function main(templateDatas, id) {
         for (var j = 0; j < templateData.inputs.length; j++) {
             var element = templateData.inputs[j];
             userPrompts.push(UserPromptFromText(i, j, element));
-
-            var answer = '';
-            var attempts = 0;
-            do {
-                attempts++;
-                if (attempts > 2) {
-                    alert('You must enter a value for ' + element + ' to proceed.');
-                }
-                answer = prompt('Story ' + (i + 1) + '/' + templateDatas.length + ': Blank ' + (j + 1) + '/' + templateData.inputs.length + '\nPlease input a ' + element)
-            }
-            while (!answer || answer.length == 0);
-            if (highlightUserWords){
-                answer = '<span class="user_answer">' + answer + '</span>';
-            }
-            templateData.string = templateData.string.replace(element, answer);
+            templateData.string = templateData.string.replace(element, userPrompts[userPrompts.length - 1].id);
         }
+    }
 
+    //Phase 2: Collect user input.
+    for (var i=0; i<userPrompts.length; i++){
+        var answer = '';
+        var attempts = 0;
+        do {
+            attempts++;
+            if (attempts > 2) {
+                alert('You must enter a value for ' + userPrompts[i].prompt + ' to proceed.');
+            }
+            answer = prompt('Item ' + (i + 1) + '/' + userPrompts.length + '\nPlease input a ' + userPrompts[i].prompt)
+        }
+        while (!answer || answer.length == 0);
+        if (highlightUserWords){
+            answer = '<span class="user_answer">' + answer + '</span>';
+        }
+        userPrompts[i].userResponse = answer;
+    }
+
+    //Phase 3: Add the user responses into the text.
+    for (var i = 0; i < userPrompts.length; i++) {
+        templateDatas[userPrompts[i].storyIndex].string =
+            templateDatas[userPrompts[i].storyIndex].string.replace(userPrompts[i].id, userPrompts[i].userResponse);
+    }
+
+    //Phase 4: Assemble the stories for output.
+    for (var i = 0; i < templateDatas.length; i++) {
+        var templateData = templateDatas[i];
         var story = createElement('div', { 'class': 'story' });
         var title = createElement('h1', {}, 'Story ' + (i + 1));
         var subtitle = createElement('h3', {}, 'From: ' + templateData.name);

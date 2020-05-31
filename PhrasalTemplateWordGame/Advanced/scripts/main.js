e5a7626c89e43ded68de456f20adb5e38b41646e
Brian Haddad <brianhaddad@gmail.com>
2020-05-31 13:23:21 -0700

    Enabled handling multiple stories at once.

--- PhrasalTemplateWordGame/Advanced/scripts/main.js
+++ PhrasalTemplateWordGame/Advanced/scripts/main.js
@@ -1,36 +1,42 @@
-function main(templateDatas){
-    var templateData = templateDatas[0]; //Temporary
-    //TODO: handle all files in a loop.
-    if (!templateData.inputs || templateData.inputs.length < 1){
-        alert('Error: This template appears to be invalid. Please select a valid template file for the game.');
-        return null;
-    }
-    for (var i=0; i<templateData.inputs.length; i++){
-        var element = templateData.inputs[i];
-        var answer = '';
-        var attempts = 0;
-        do {
-            attempts++;
-            if (attempts > 2){
-                alert('You must enter a value for ' + element + ' to proceed.');
-            }
-            answer = prompt('(' + (i + 1) + '/' + templateData.inputs.length + ') Please input a ' + element)
-        }
-        while (!answer || answer.length == 0);
-        templateData.string = templateData.string.replace(element, answer);
-    }
+function main(templateDatas) {
     var container = document.getElementById('main');
     var linebreak = '[linebreak]';
-    templateData.string = templateData.string.replace(/(\r\n|\n|\r)/gm, linebreak);
-    var lines = templateData.string.split(linebreak);
-    var story = createElement('div', {'class':'story'});
-    var title = createElement('h2', {}, 'Story');
-    story.appendChild(title);
-    for (var i=0; i<lines.length; i++){
-        var p = createElement('p', {}, lines[i]);
-        story.appendChild(p);
+
+    for (var i = 0; i < templateDatas.length; i++) {
+        var templateData = templateDatas[i];
+        if (!templateData.inputs || templateData.inputs.length < 1) {
+            alert('Error: This template appears to be invalid. Please select a valid template file for the game.');
+            return null;
+        }
+
+        for (var j = 0; j < templateData.inputs.length; j++) {
+            var element = templateData.inputs[j];
+            var answer = '';
+            var attempts = 0;
+            do {
+                attempts++;
+                if (attempts > 2) {
+                    alert('You must enter a value for ' + element + ' to proceed.');
+                }
+                answer = prompt('Story ' + (i + 1) + '/' + templateDatas.length + ': Blank ' + (j + 1) + '/' + templateData.inputs.length + '\nPlease input a ' + element)
+            }
+            while (!answer || answer.length == 0);
+            templateData.string = templateData.string.replace(element, answer);
+        }
+
+        var story = createElement('div', { 'class': 'story' });
+        var title = createElement('h2', {}, 'Story ' + (i + 1));
+        story.appendChild(title);
+
+        templateData.string = templateData.string.replace(/(\r\n|\n|\r)/gm, linebreak);
+        var lines = templateData.string.split(linebreak);
+        for (var j = 0; j < lines.length; j++) {
+            var p = createElement('p', {}, lines[j]);
+            story.appendChild(p);
+        }
+        
+        container.appendChild(story);
     }
-    container.appendChild(story);
     container.style.display = 'block';
 }
 
